85aceddbf97128fdc727f1f496f7b1706dcde316
Razus228 <cassilasq2@gmail.com>
2025-02-04 22:58:33 +0200

    inspired by tutorial

---
+++ Lab6/main.py
@@ -0,0 +1,31 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+def check_mood(text):
+
+
+    analyzer = SentimentIntensityAnalyzer()
+
+    mood_score = analyzer.polarity_scores(text)
+
+    if mood_score['compound'] >= 0.05:
+        mood = "OMG, YOU PASSED, but not so fast, you got one more challenge ahead"
+
+    elif mood_score['compound'] <= -0.05:
+        mood = "BOOOOOOOO, SEE YOU NEXT YEAR LOSER"
+
+    else: 
+        mood = "I guess.... everything is alright??!!" 
+
+    return mood, mood_score
+
+def main():
+
+    text = input("Give me my verdict... ")
+
+    mood, score = check_mood(text)
+
+    print(f"Your verdict: {mood}")
+    print(f"Mood Score: {score}")
+  
+if __name__ == "__main__":
+    main()
